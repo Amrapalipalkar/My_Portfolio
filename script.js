d912000ce47c1d70d386abaa60d08e1d965d600d
Er. Amrapali Palkar <80786215+Amrapalipalkar@users.noreply.github.com>
2023-03-07 12:44:15 +0530

    update script

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer", "React Developer", "Frontend Developer", "Web Developer"],
+        strings: [ "Full Stack Developer", "React Developer", "Frontend Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Full Stack Developer", "React Developer", "Frontend Developer"],
+        strings: ["Full Stack Developer", "React Developer", "Frontend Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
